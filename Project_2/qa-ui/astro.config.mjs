62bff103feacd8730d637fc5a86fe015f08fc740
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2023-10-27 21:05:09 +0300

    Finally working prod build

--- Project_2/qa-ui/astro.config.mjs
+++ Project_2/qa-ui/astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
 
+// https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [svelte(), tailwind()],
   server: {
     port: 3000,
     host: true,
   },
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
