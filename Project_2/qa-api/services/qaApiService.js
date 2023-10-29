70a05c9ea2b54e2f69d6ec3c53c7a89437901e76
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2023-10-29 19:21:22 +0200

    redis caching

--- Project_2/qa-api/services/qaApiService.js
+++ Project_2/qa-api/services/qaApiService.js
@@ -1,22 +1,44 @@
 import * as database from '../database/database.js'
 import { pubClient } from './pubService.js'
 import { subClient } from './subService.js'
+import { createClient } from '../deps.js'
+
+export const redis = createClient({
+  url: 'redis://redis:6379',
+  pingInterval: 1000,
+})
+
+await redis.connect()
 
 export const question_timeouts = new Set()
 export const answer_timeouts = new Set()
 
 const handleGetAllCourses = async () => {
+  const cache = await redis.get(`courses`)
+  if (cache) {
+    console.log('redis')
+    return JSON.parse(cache)
+  }
   const courses = await database.getCourses()
+  redis.set(`courses`, JSON.stringify(courses))
   return courses
 }
 
 const handleGetCourse = async (id, user_id) => {
-  const res = await database.getCourse(id)
-
-  if (!res) {
-    return null
+  let course = null
+  const cache = await redis.get(`course_${id}`)
+  if (cache) {
+    console.log('redis')
+    course = JSON.parse(cache)
+  } else {
+    const res = await database.getCourse(id)
+    if (!res) {
+      return null
+    }
+    redis.set(`course_${id}`, JSON.stringify(res[0]))
+    course = res[0]
   }
-  const course = res[0]
+
   const questions = await database.getQuestions(course.course_id, 0)
 
   const questionIds = questions.map((question) => question.question_id)
@@ -46,13 +68,19 @@ const handleGetCourse = async (id, user_id) => {
 }
 
 const handleGetQuestions = async (id, user_id, page) => {
-  const res = await database.getCourse(id)
-
-  if (!res) {
-    return null
+  let course = null
+  const cache = await redis.get(`course_${id}`)
+  if (cache) {
+    console.log('redis')
+    course = JSON.parse(cache)
+  } else {
+    const res = await database.getCourse(id)
+    if (!res) {
+      return null
+    }
+    redis.set(`course_${id}`, JSON.stringify(res[0]))
+    course = res[0]
   }
-
-  const course = res[0]
   const questions = await database.getQuestions(course.course_id, page)
 
   const questionIds = questions.map((question) => question.question_id)
@@ -79,12 +107,20 @@ const handleGetQuestions = async (id, user_id, page) => {
 }
 
 const handleGetQuestion = async (id, user_id) => {
-  const res = await database.getQuestion(id)
-
-  if (!res) {
-    return null
+  let question = null
+  const cache = await redis.get(`question_${id}`)
+  if (cache) {
+    console.log('redis')
+    question = JSON.parse(cache)
+  } else {
+    const res = await database.getQuestion(id)
+    if (!res) {
+      return null
+    }
+    redis.set(`question_${id}`, JSON.stringify(res[0]))
+    question = res[0]
   }
-  const question = res[0]
+
   const answers = await database.getAnswers(question.question_id, 0)
 
   const answerIds = answers.map((answer) => answer.answer_id)
@@ -114,13 +150,20 @@ const handleGetQuestion = async (id, user_id) => {
 }
 
 const handleGetAnswers = async (id, user_id, page) => {
-  const res = await database.getQuestion(id)
-
-  if (!res) {
-    return null
+  let question = null
+  const cache = await redis.get(`question_${id}`)
+  if (cache) {
+    console.log('redis')
+    question = JSON.parse(cache)
+  } else {
+    const res = await database.getQuestion(id)
+    if (!res) {
+      return null
+    }
+    redis.set(`question_${id}`, JSON.stringify(res[0]))
+    question = res[0]
   }
 
-  const question = res[0]
   const answers = await database.getAnswers(question.question_id, page)
 
   const answerIds = answers.map((answer) => answer.answer_id)
@@ -177,6 +220,7 @@ const handlePostAnswer = async (question_id, answer, user) => {
       data: { ...res[0], upvotes: 0, user_upvoted: false },
     })
   )
+  await redis.del(`question_${question_id}`)
   return true
 }
 
@@ -185,6 +229,11 @@ const handlePostUpvote = async (question_id, answer_id, user) => {
     throw new Error('Missing required fields')
   }
   const res = await database.addUpvote(question_id, answer_id, user)
+  if (question_id) {
+    await redis.del(`question_${question_id}`)
+  } else if (answer_id) {
+    await redis.del(`answer_${answer_id}`)
+  }
   return res[0]
 }
 
