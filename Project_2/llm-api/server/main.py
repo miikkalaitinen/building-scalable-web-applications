018670a0bdf330abceb9ac707928f2e22a731907
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2023-11-01 15:46:00 +0200

    Add async back to fastapi

--- Project_2/llm-api/server/main.py
+++ Project_2/llm-api/server/main.py
@@ -4,11 +4,11 @@ from .models import generator
 app = FastAPI()
 
 @app.get("/")
-def main():
+async def main():
     return "POST a message with a JSON document that has a 'question' key."
 
 @app.post("/")
-def ask_question(request: Request):
-    data = request.json()
+async def ask_question(request: Request):
+    data = await request.json()
     print("Received question!")
     return generator(data["question"])
